8c8295cd3a093a0d784b9f66ab8ec5e7de749544
Prajwal G <104543731+PrajwalGiridhar@users.noreply.github.com>
2022-05-10 15:31:37 +0530

    Update MasterDemo.java

--- src/com/capgemini/masterDemo/MasterDemo.java
+++ src/com/capgemini/masterDemo/MasterDemo.java
@@ -5,6 +5,8 @@ public class MasterDemo
 	public static void main(String[] args)
 	{
 		System.out.println("this is master demo");
+		System.out.println("this is master");
+		System.out.println("this is demo");
 		
 	}
 
